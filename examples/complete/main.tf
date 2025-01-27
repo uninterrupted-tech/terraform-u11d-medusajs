7c9a283b82d6008c36d6651b4d573c1b5cf671bf
Maciej Łopalewski <maciej.lopalewski@uninterrupted.tech>
2025-01-27 23:43:13 +0100

    Add docs

---
+++ examples/complete/main.tf
@@ -0,0 +1,136 @@
+terraform {
+  required_version = "~> 1.10.0"
+}
+
+locals {
+  project     = "medusa-demo"
+  environment = "prod"
+}
+
+provider "aws" {
+  region = "eu-west-1"
+  default_tags {
+    tags = {
+      Project     = local.project
+      Environment = local.environment
+      Terraform   = true
+    }
+  }
+}
+
+module "complete" {
+  source = "u11d-com/terraform-u11d-medusajs"
+
+  # Global
+  project     = local.project
+  environment = local.environment
+
+  # ECR
+  ecr_backend_create             = true
+  ecr_backend_retention_count    = 32
+  ecr_storefront_create          = true
+  ecr_storefront_retention_count = 32
+
+  # Network
+  vpc_create = true
+  cidr_block = "10.0.0.0/16"
+  az_count   = 2
+
+  # Redis (Elasticache)
+  elasticache_create               = true
+  elasticache_node_type            = "cache.t3.micro"
+  elasticache_nodes_num            = 1
+  elasticache_redis_engine_version = "7.1"
+  elasticache_port                 = 6379
+
+  # Database (RDS)
+  rds_create            = true
+  rds_username          = "medusa"
+  rds_instance_class    = "db.t3.micro"
+  rds_allocated_storage = 5
+  rds_engine_version    = "15.7"
+  rds_port              = 5432
+
+  # Backend
+  backend_create         = true
+  backend_container_port = 9000
+  backend_target_group_health_check_config = {
+    interval            = 30
+    matcher             = 200
+    timeout             = 3
+    path                = "/health"
+    healthy_threshold   = 3
+    unhealthy_threshold = 3
+  }
+  backend_cloudfront_price_class = "PriceClass_100"
+  backend_expose_admin_only      = false
+  backend_container_image        = "your-backend-image:latest"
+  backend_resources = {
+    instances = 1
+    cpu       = 2048
+    memory    = 4096
+  }
+  backend_logs = {
+    group     = "/medusa-backend"
+    retention = 30
+    prefix    = "container"
+  }
+  backend_run_migrations           = true
+  backend_seed_create              = true
+  backend_seed_run                 = true
+  backend_seed_command             = "npx medusa seed -f ./data/seed.json"
+  backend_seed_timeout             = 60
+  backend_seed_fail_on_error       = true
+  backend_extra_security_group_ids = []
+  backend_extra_environment_variables = {
+    "NODE_ENV" = "development"
+  }
+  backend_extra_secrets = {}
+
+  # Storefront
+  storefront_create         = true
+  storefront_container_port = 8000
+  storefront_target_group_health_check_config = {
+    interval            = 30
+    matcher             = 200
+    timeout             = 3
+    path                = "/api"
+    healthy_threshold   = 3
+    unhealthy_threshold = 3
+  }
+  storefront_cloudfront_price_class = "PriceClass_100"
+  storefront_container_image        = "your-storefront-image:latest"
+  storefront_container_registry_credentials = {
+    username = "your-registry-username"
+    password = "your-registry-password"
+  }
+  storefront_resources = {
+    instances = 1
+    cpu       = 1024
+    memory    = 2048
+  }
+  storefront_logs = {
+    group     = "/medusa-storefront"
+    retention = 30
+    prefix    = "container"
+  }
+  storefront_extra_security_group_ids    = []
+  storefront_extra_environment_variables = {}
+  storefront_extra_secrets               = {}
+}
+
+output "ecr_backend_url" {
+  value = module.complete.ecr_backend_url
+}
+
+output "ecr_storefront_url" {
+  value = module.complete.ecr_storefront_url
+}
+
+output "backend_url" {
+  value = module.complete.backend_url
+}
+
+output "storefront_url" {
+  value = module.complete.storefront_url
+}
